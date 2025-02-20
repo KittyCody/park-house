3fdfab64e22a924471ace8d738bede7be7657dc0
Antony Jekov <antonyjekov@proton.me>
2025-02-20 16:56:03 +0100

    adds integration tests maven plugin

--- src/test/java/io/kittycody/parking/ParkingApplicationTests.java
+++ src/test/java/io/kittycody/parking/ParkingApplicationTests.java
@@ -2,8 +2,10 @@ package io.kittycody.parking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ParkingApplicationTests {
 
